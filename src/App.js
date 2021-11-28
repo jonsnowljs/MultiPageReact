c5f02948723bbcd28b9cadc5a4fcff439cf5ee6e
Jiasheng Lu <jonsnowljs@gmail.com>
2021-11-27 23:20:34 -0700

    add redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 import AllQuote from './pages/AllQuote';
 import NewQuote from './pages/NewQuote';
 import QuoteDetail from './pages/QuoteDetail';
@@ -6,7 +6,9 @@ import QuoteDetail from './pages/QuoteDetail';
 function App() {
   return (
     <Switch>
-      
+      <Route path='/' exact>
+        <Redirect to='quotes'/>
+      </Route>
       <Route path='/quotes' exact>
         <AllQuote/>
       </Route>
