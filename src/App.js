f489b804f78238835c41efbc17ed536b62909e56
Jiasheng Lu <jonsnowljs@gmail.com>
2021-11-29 14:56:35 -0700

    Add not found page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import {Route, Switch, Redirect} from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom';
 import AllQuote from './pages/AllQuote';
 import NewQuote from './pages/NewQuote';
 import QuoteDetail from './pages/QuoteDetail';
+import Layout from './components/layout/Layout';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Switch>
-      <Route path='/' exact>
-        <Redirect to='quotes'/>
-      </Route>
-      <Route path='/quotes' exact>
-        <AllQuote/>
-      </Route>
-      <Route path='/quotes/:quoteId'>
-        <QuoteDetail/>
-      </Route>
-      <Route path='/new-quote'>
-        <NewQuote/>
-      </Route>
-      
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuote />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+        <Route path='*'>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
